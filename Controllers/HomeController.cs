f031148870d3e9546ed9a8213e92eed0b3ed5c33
Bhumi vekariya <bhumivekariya694@gmail.com>
2024-11-14 21:59:08 +0530

    initial commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RMSByInfinitix.Controllers
+{
+	public class HomeController : Controller
+	{
+		public ActionResult LogIn()
+		{
+			return View();	
+		}
+
+		public ActionResult Dashboard()
+		{
+			return View();	
+		}
+
+		public ActionResult ClientIndex()
+		{
+			return View();	
+		}
+
+		public ActionResult ClientAddEdit()
+		{
+			return View();	
+		}
+	}
+}
